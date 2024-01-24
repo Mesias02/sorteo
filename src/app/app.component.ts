a4a0b831864ffac1a69df2f47c66ae45aa2a3902
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2024-01-23 20:56:46 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './components/head/head.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
